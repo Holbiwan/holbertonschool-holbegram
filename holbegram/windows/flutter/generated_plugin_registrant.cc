297663e57dbdc8e9627020055039005c5bd0b580
Holbiwan <sabrinapapeau@gmail.com>
2024-12-01 20:11:49 +0100

    Update dependencies and run app for final checks

--- holbegram/windows/flutter/generated_plugin_registrant.cc
+++ holbegram/windows/flutter/generated_plugin_registrant.cc
@@ -2,7 +2,6 @@
 //  Generated file. Do not edit.
 //
 
-// Disable automatic formatting for this file
 // clang-format off
 
 #include "generated_plugin_registrant.h"
@@ -13,26 +12,15 @@
 #include <firebase_core/firebase_core_plugin_c_api.h>
 #include <firebase_storage/firebase_storage_plugin_c_api.h>
 
-// Function to register all necessary plugins with the Flutter plugin registry
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  // Register the Cloud Firestore plugin
   CloudFirestorePluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("CloudFirestorePluginCApi"));
-  
-  // Register the File Selector plugin for Windows
   FileSelectorWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FileSelectorWindows"));
-  
-  // Register the Firebase Authentication plugin
   FirebaseAuthPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FirebaseAuthPluginCApi"));
-  
-  // Register the Firebase Core plugin
   FirebaseCorePluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FirebaseCorePluginCApi"));
-  
-  // Register the Firebase Storage plugin
   FirebaseStoragePluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FirebaseStoragePluginCApi"));
 }
-
