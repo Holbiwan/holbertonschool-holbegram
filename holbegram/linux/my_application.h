54a984bde54754b418aa3481c55be7e16d63abad
Holbiwan <sabrinapapeau@gmail.com>
2024-12-01 23:35:07 +0100

    Fix my_application files

--- holbegram/linux/my_application.h
+++ holbegram/linux/my_application.h
@@ -2,7 +2,17 @@
 #define FLUTTER_MY_APPLICATION_H_
 
 #include <gtk/gtk.h>
+#include <glib-object.h>
 
+/*
+ * MyApplication:
+ *
+ * A custom GTK-based application for running a Flutter engine.
+ * This class is derived from GtkApplication and integrates
+ * Flutter capabilities into a GTK-based application framework.
+ */
+
+/* Declare MyApplication as a final type inheriting from GtkApplication */
 G_DECLARE_FINAL_TYPE(MyApplication, my_application, MY, APPLICATION,
                      GtkApplication)
 
@@ -10,8 +20,12 @@ G_DECLARE_FINAL_TYPE(MyApplication, my_application, MY, APPLICATION,
  * my_application_new:
  *
  * Creates a new Flutter-based application.
+ * 
+ * This function initializes an instance of `MyApplication`,
+ * which is configured to embed and manage a Flutter engine
+ * alongside GTK features.
  *
- * Returns: a new #MyApplication.
+ * Returns: (transfer full): A new #MyApplication instance.
  */
 MyApplication* my_application_new();
 
