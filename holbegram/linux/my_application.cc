950f163023db030efb32361837991b265d2f278a
Holbiwan <sabrinapapeau@gmail.com>
2024-12-01 21:39:15 +0100

    Improve readaptibility

--- holbegram/linux/my_application.cc
+++ holbegram/linux/my_application.cc
@@ -14,19 +14,14 @@ struct _MyApplication {
 
 G_DEFINE_TYPE(MyApplication, my_application, GTK_TYPE_APPLICATION)
 
-// Implements GApplication::activate.
+// Implements GApplication::activate
 static void my_application_activate(GApplication* application) {
   MyApplication* self = MY_APPLICATION(application);
-  GtkWindow* window =
-      GTK_WINDOW(gtk_application_window_new(GTK_APPLICATION(application)));
 
-  // Use a header bar when running in GNOME as this is the common style used
-  // by applications and is the setup most users will be using (e.g. Ubuntu
-  // desktop).
-  // If running on X and not using GNOME then just use a traditional title bar
-  // in case the window manager does more exotic layout, e.g. tiling.
-  // If running on Wayland assume the header bar will work (may need changing
-  // if future cases occur).
+  // Create a new GTK window for the application
+  GtkWindow* window = GTK_WINDOW(gtk_application_window_new(GTK_APPLICATION(application)));
+
+  // Determine if a header bar should be used
   gboolean use_header_bar = TRUE;
 #ifdef GDK_WINDOWING_X11
   GdkScreen* screen = gtk_window_get_screen(window);
@@ -37,19 +32,22 @@ static void my_application_activate(GApplication* application) {
     }
   }
 #endif
+
+  // Configure the header bar or traditional title bar
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
-    gtk_widget_show(GTK_WIDGET(header_bar));
     gtk_header_bar_set_title(header_bar, "holbegram");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
+    gtk_widget_show(GTK_WIDGET(header_bar));
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
     gtk_window_set_title(window, "holbegram");
   }
 
+  // Set default window size
   gtk_window_set_default_size(window, 1280, 720);
-  gtk_widget_show(GTK_WIDGET(window));
 
+  // Create and configure the Flutter view
   g_autoptr(FlDartProject) project = fl_dart_project_new();
   fl_dart_project_set_dart_entrypoint_arguments(project, self->dart_entrypoint_arguments);
 
@@ -57,55 +55,62 @@ static void my_application_activate(GApplication* application) {
   gtk_widget_show(GTK_WIDGET(view));
   gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(view));
 
+  // Register plugins for the Flutter view
   fl_register_plugins(FL_PLUGIN_REGISTRY(view));
 
+  // Set focus to the Flutter view
   gtk_widget_grab_focus(GTK_WIDGET(view));
+
+  // Show the window
+  gtk_widget_show(GTK_WIDGET(window));
 }
 
-// Implements GApplication::local_command_line.
-static gboolean my_application_local_command_line(GApplication* application, gchar*** arguments, int* exit_status) {
+// Implements GApplication::local_command_line
+static gboolean my_application_local_command_line(
+    GApplication* application, gchar*** arguments, int* exit_status) {
   MyApplication* self = MY_APPLICATION(application);
-  // Strip out the first argument as it is the binary name.
+
+  // Extract command-line arguments, excluding the binary name
   self->dart_entrypoint_arguments = g_strdupv(*arguments + 1);
 
   g_autoptr(GError) error = nullptr;
   if (!g_application_register(application, nullptr, &error)) {
-     g_warning("Failed to register: %s", error->message);
-     *exit_status = 1;
-     return TRUE;
+    g_warning("Failed to register: %s", error->message);
+    *exit_status = 1;
+    return TRUE;
   }
 
+  // Activate the application
   g_application_activate(application);
   *exit_status = 0;
 
   return TRUE;
 }
 
-// Implements GApplication::startup.
+// Implements GApplication::startup
 static void my_application_startup(GApplication* application) {
-  //MyApplication* self = MY_APPLICATION(object);
-
-  // Perform any actions required at application startup.
-
+  // Call the parent class's startup method
   G_APPLICATION_CLASS(my_application_parent_class)->startup(application);
 }
 
-// Implements GApplication::shutdown.
+// Implements GApplication::shutdown
 static void my_application_shutdown(GApplication* application) {
-  //MyApplication* self = MY_APPLICATION(object);
-
-  // Perform any actions required at application shutdown.
-
+  // Call the parent class's shutdown method
   G_APPLICATION_CLASS(my_application_parent_class)->shutdown(application);
 }
 
-// Implements GObject::dispose.
+// Implements GObject::dispose
 static void my_application_dispose(GObject* object) {
   MyApplication* self = MY_APPLICATION(object);
+
+  // Free the dart_entrypoint_arguments array
   g_clear_pointer(&self->dart_entrypoint_arguments, g_strfreev);
+
+  // Call the parent class's dispose method
   G_OBJECT_CLASS(my_application_parent_class)->dispose(object);
 }
 
+// Class initialization function
 static void my_application_class_init(MyApplicationClass* klass) {
   G_APPLICATION_CLASS(klass)->activate = my_application_activate;
   G_APPLICATION_CLASS(klass)->local_command_line = my_application_local_command_line;
@@ -114,8 +119,10 @@ static void my_application_class_init(MyApplicationClass* klass) {
   G_OBJECT_CLASS(klass)->dispose = my_application_dispose;
 }
 
+// Instance initialization function
 static void my_application_init(MyApplication* self) {}
 
+// Factory method to create a new MyApplication instance
 MyApplication* my_application_new() {
   return MY_APPLICATION(g_object_new(my_application_get_type(),
                                      "application-id", APPLICATION_ID,
